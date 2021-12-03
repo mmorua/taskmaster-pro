70bc4d7769fab803eda9bf5a2be4cc34a38c94de
Marcus Morua <marcus.morua@gmail.com>
2021-12-03 15:24:22 -0600

    style : UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,15 +82,23 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event, ui) {
     console.log(ui);
+    $(this).addClass("dropover");
+    $("bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event, ui) {
     console.log(ui);
+    $(this).removeClass("dropover");
+    $("bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
     console.log(event);
+    $(event.target).addClass("dropover-active");
+    $("bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event) {
     console.log(event);
+    $(event.target).removeClass("dropover-active");
+    $("bottom-trash").removeClass("bottom-trash-active");
   },
   update: function() {
     var tempArr = [];
@@ -161,7 +169,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
